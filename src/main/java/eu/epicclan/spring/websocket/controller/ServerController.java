1d9c9f2ef6a9a277a95e4a58a27753d72607ee5a
Epictigu <tigusa.r@gmail.com>
2022-06-01 15:47:42 +0200

    Default error page

--- src/main/java/eu/epicclan/spring/websocket/controller/ServerController.java
+++ src/main/java/eu/epicclan/spring/websocket/controller/ServerController.java
@@ -1,5 +1,6 @@
 package eu.epicclan.spring.websocket.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import eu.epicclan.spring.websocket.utils.ServerList;
 public class ServerController {
 	
 	@GetMapping("/api/getservers")
+	@PreAuthorize("hasRole('ADMIN')")
 	public ServerList serverList() throws Exception {
 		return new ServerList(WebsocketApplication.sFile.servers);
 	}
